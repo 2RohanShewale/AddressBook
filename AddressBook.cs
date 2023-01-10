b0680737d390a04ef1eb4659c35d37d6e69d2b1a
2RohanShewale <shewalerohan50@gmail.com>
2023-01-10 11:00:25 +0530

    Add multiple contacts in one addressbook

--- AddressBook.cs
+++ AddressBook.cs
@@ -1,27 +1,41 @@
-﻿using System;
+﻿using AddressBook;
+using System;
 using static System.Console;
 
 namespace AddressBookMain
 {
     internal class AddressBook
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? address { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? Country { get; set; }
-        public int ZipCode { get; set; }
-        public int PhoneNumber { get; set; }
-        public string? Email { get; set; }
+        public List<Contact> Contacts = new List<Contact>();
+        
 
+        public void CreateContact()
+        {
+            WriteLine("---Add New Contact---");
+            Write("First Name: "); string? FName = ReadLine();
+            Write("Last Name: "); string? LName = ReadLine();
+            Write("Address: "); string? address = ReadLine();
+            Write("City: "); string? city = ReadLine();
+            Write("State: "); string? state = ReadLine();
+            Write("Country: "); string? country = ReadLine();
+            Write("Zip Code: "); int code = Convert.ToInt32(ReadLine());
+            Write("Phone Number: "); int number = Convert.ToInt32(ReadLine());
+            Write("Email: "); string? email = ReadLine();
+            Contact contact= new Contact(FirstName: FName, LastName: LName, Address: address, City: city, State: state, Country: country, ZipCode: code, PhoneNumber: number, Email: email);
+            Contacts.Add(contact);
+            Display();
+        }
         public void Display()
         {
-            WriteLine("Name: " + this.FirstName + " " + this.LastName);
-            WriteLine("Address: " + this.address + " " + this.City + " " + this.City + " " + this.Country + " " + this.ZipCode);
-            WriteLine("Phone Number: " + this.PhoneNumber);
-            WriteLine("Email: " + this.Email);
-            WriteLine("*--------------------*");
+            foreach (var contact in Contacts)
+            {
+                WriteLine();
+                WriteLine("Name: " + contact.FirstName + " " + contact.LastName);
+                WriteLine("Address: " + contact.Address + " " + contact.City + " " + contact.City + " " + contact.Country + " " + contact.ZipCode);
+                WriteLine("Phone Number: " + contact.PhoneNumber);
+                WriteLine("Email: " + contact.Email);
+                WriteLine("*--------------------*");
+            }
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -5,19 +5,15 @@
 		static void Main()
 		{
 			Console.WriteLine("#------Welcome to Address Book Program------#");
-			AddressBook contact = new AddressBook() {
-				FirstName= "Rohan",
-				LastName = "Shewale",
-				address = "Room: 237, 13th floor",
-				City = "Mumbai",
-				State = "Maharastra",
-				Country = "India",
-				ZipCode = 400215,
-				PhoneNumber = 98695421,
-				Email = "rohan@gmail.com"	
-			};
-			contact.Display();
-			
+			AddressBook book1 = new AddressBook();
+			bool addContact = true;
+			while (addContact)
+			{
+                book1.CreateContact();
+				Console.Write("Do you want to add more contact?(Y/N) :");
+				string? s = Console.ReadLine();
+				if (s == "N" || s == "n") { addContact = false; }
+            }	
 		}
 	}
 }
