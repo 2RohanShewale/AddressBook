437522647f38deccef56bd0c86e5848bd8c3e187
2RohanShewale <shewalerohan50@gmail.com>
2023-01-10 10:10:32 +0530

    AddContact

---
+++ AddressBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace AddressBookMain
+{
+    internal class AddressBook
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public int ZipCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public string? Email { get; set; }
+
+        public void Display()
+        {
+            WriteLine("Name: " + this.FirstName + " " + this.LastName);
+            WriteLine("Address: " + this.address + " " + this.City + " " + this.City + " " + this.Country + " " + this.ZipCode);
+            WriteLine("Phone Number: " + this.PhoneNumber);
+            WriteLine("Email: " + this.Email);
+            WriteLine("*--------------------*");
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
-﻿namespace AddressBook
+﻿namespace AddressBookMain
 {
 	class Program
 	{
 		static void Main()
 		{
 			Console.WriteLine("#------Welcome to Address Book Program------#");
+			AddressBook contact = new AddressBook() {
+				FirstName= "Rohan",
+				LastName = "Shewale",
+				address = "Room: 237, 13th floor",
+				City = "Mumbai",
+				State = "Maharastra",
+				Country = "India",
+				ZipCode = 400215,
+				PhoneNumber = 98695421,
+				Email = "rohan@gmail.com"	
+			};
+			contact.Display();
+			
 		}
 	}
 }
